9f9d88fc2e46ac1ff7ba2ea28c7f64bd82c6be36
CarinaGhindea <ghindeacarina@gmail.com>
2026-09-15 16:09:14 -0400

    Update skeleton.ino
    Updated skeleton code to include new bonus activities

--- Part2_Pong/implementing_pong/skeleton/skeleton.ino
+++ Part2_Pong/implementing_pong/skeleton/skeleton.ino
@@ -15,11 +15,13 @@ const int BTN_P2 = 4;
 bool lastBtnP1 = HIGH;
 bool lastBtnP2 = HIGH;
 
-int p1Dir = 0;
-int p2Dir = 0;
+const int p1Dir = 0;
+const int p2Dir = 0;
 
 unsigned long  gameTime = 0;
 unsigned long  startTime = 0;
+
+// don't forget to add definitions as you go through the bonuses!
 
 enum GameState {
   // Don't worry about game states for now! 
@@ -47,11 +49,20 @@ void loop() {
 
   //############################################################## 
   // - input button functionality from the instructions
-  // CHALLENGE: add constraints to the paddles so they don't leave the screen
+  
+  // BONUS: add constraints to the paddles so they don't leave the screen
+  // BONUS: start the game when both players are ready 
+  // BONUS: add ball movement
+  // BONUS: collision with ball and paddles 
+  // BONUS: what should happen when the ball hits the top and bottom of the screen? 
 
 
   //############################################################## 
-  // - add your code from the timer module challenge
+  // - add your code from the timer module challenge 
+
+  // BONUS: have the timer start when both players are ready 
+  // BONUS: keep track of players' score
+  // BONUS: create win criteria 
 
 
   //############################################################## 
@@ -64,7 +75,11 @@ void loop() {
 }
 
 void resetGame(){
-  //CHALLENGE
-  // - reset the game! 
+  // BONUS: reset the game with a pushbutton!
+
+}
+
+void resetRound(){
+  // BONUS: reset the round after a point is scored!
 
 }
